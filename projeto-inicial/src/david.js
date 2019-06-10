dee1a585df9d43dae3d1541730eb8d3c0836c5d8
David Samerson <samerson10@gmail.com>
2019-06-10 16:07:01 -0300

    criando componentes

---
+++ projeto-inicial/src/david.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class David extends Component {
+  render () {
+    return( 
+    <div className="App">
+        <p>David</p>
+    </div>
+    );
+
+  }
+
+}
+
+export default David;

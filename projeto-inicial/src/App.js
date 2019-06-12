da09f138422519b3c2e81ade1de09facb03a8235
David Samerson <samerson10@gmail.com>
2019-06-12 15:52:54 -0300

    alguns erros

--- projeto-inicial/src/App.js
+++ projeto-inicial/src/App.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
+import {Link, Route} from 'react-router-dom';
+import {routesConfig} from './routesConfig';
+
 import './App.css';
-import David from './david';
-import User from './User';
-import foto from './imagem.png';
-import Lista from './Lista';
+
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/user">User</Link>
+        </div>
+        {routesConfig.map((value, key) => {
+          return <Route
+            key={key}
+            path={value.path}
+            component={value.component}
+            exact={value.exact}
+          ></Route>
+        })
+        }
       </div>
     );
   }
